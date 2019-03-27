e2c7e491c83ec4b0f5dd7bf98ffe2f3cfc9d8ed5
PJ Joshi <joshipv2@gmail.com>
2019-03-27 00:11:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 function removeLastKitten() {
-  kittens.slice(0, kittens.length-1)
+  return kittens.slice(0, kittens.length-1)
 }
 function removeFirstKitten() {
-  kittens.slice(-1, length-1)
+  return kittens.slice(-1, length-1)
 }
