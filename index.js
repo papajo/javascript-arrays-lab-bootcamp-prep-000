bee048f4ced381d7729c506d0cac2ea4227abb43
PJ Joshi <joshipv2@gmail.com>
2019-03-27 00:10:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,17 +8,20 @@ function destructivelyPrependKitten(name) {
   kittens.unshift(name)
 }
 function destructivelyRemoveLastKitten() {
-  
+  kittens.pop()
 }
 function destructivelyRemoveFirstKitten() {
-  
+  kittens.shift()
 }
 function appendKitten(name) {
-  
+  return [...kittens, name]
 }
 function prependKitten(name) {
-  
+  return [name, ...kittens]
 }
 function removeLastKitten() {
-  
+  kittens.slice(0, kittens.length-1)
+}
+function removeFirstKitten() {
+  kittens.slice(-1, length-1)
 }
